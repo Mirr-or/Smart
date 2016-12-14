0ba71aa2cf294fe1dc77a65d1d41902647c0ce44
Mirr-or <Mirovinger@uvaga.by>
2016-12-14 21:35:16 +0400

    small up

--- routers.php
+++ routers.php
@@ -4,3 +4,10 @@
 $routers->get('task-list', 'TodoController@index');
 $routers->post('task/add', 'TodoController@add');
 $routers->post('task/update', 'TodoController@update');
+$routers->post('task/delete', 'TodoController@delete');
+
+$routers->get('', 'PageController@index');
+
+$routers->get('table', 'PageController@table');
+
+$routers->get('about', 'PageController@about');
